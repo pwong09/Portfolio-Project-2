037afe9b8c2094415f6cec0b2919f6dde836b46f
pwong09 <91284556+pwong09@users.noreply.github.com>
2021-12-14 13:37:13 -0800

    add quicksort file

--- script.py
+++ script.py
@@ -2,3 +2,10 @@ import utils
 
 pearls_bookshelf = utils.load_books('books-small.csv')
 
+print("*********************")
+print("****** WELCOME ******")
+print("** TO *** PEARL'S ***")
+print("***** BOOKSHELF *****")
+
+print("What are you looking for today?")
+browse_category = input("Type the beginning of a category you would like to browse.")
---
+++ utils.py
@@ -0,0 +1,16 @@
+import csv
+
+# This code loads the current book
+# shelf data from the csv file
+def load_books(filename):
+  bookshelf = []
+  with open(filename) as file:
+      shelf = csv.DictReader(file)
+      for book in shelf:
+          # add your code here
+          book['author_lower'] = book['author'].lower()
+          book['title_lower'] = book['title'].lower()
+          book['category_lower'] = book['category'].lower()
+          book['rating']
+          bookshelf.append(book)
+  return bookshelf
