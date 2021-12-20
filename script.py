b75908ebd3db91a7291f90b317f24ab3d5442a3a
pwong09 <91284556+pwong09@users.noreply.github.com>
2021-12-20 10:23:45 -0800

    recommend top rated book

--- script.py
+++ script.py
@@ -1,6 +1,5 @@
 import utils
 import sorts
-import re
 
 pearls_bookshelf = utils.load_books('books-small.csv')
 
@@ -13,12 +12,11 @@ pearls_bookshelf = utils.load_books('books-small.csv')
 #  print(book['title'] + " written by " + book['author'])
 
 def by_rating(book_a, book_b):
-    return book_a['rating'] > book_b['rating']
+    return book_a['rating'] < book_b['rating']
 
 sort_rating = sorts.quicksort(pearls_bookshelf, 0, len(pearls_bookshelf) - 1, by_rating)
 
 categories = []
-
 for book in pearls_bookshelf:
     categories.append(book['category_lower'])
 
@@ -35,20 +33,24 @@ def check(letter, list):
     list_with_letter = [i for i in list if i.startswith(letter)]
     return list_with_letter
 
-
+print("\n")
+print("\n")
 print("*********************")
 print("****** WELCOME ******")
 print("** TO *** PEARL'S ***")
 print("***** BOOKSHELF *****")
-#User Interaction loop 
+print("*********************")
+print("Hiya, friend!")
 print("What are you looking for today?")
+print("We have a variety of books in many different categories: \n")
+for category in check_categories:
+    print("{0}".format(category).upper())
+
 selected_books = []
 browse_category = ""
 short_list = []
 
-if not re.match("^[a-z]*$", browse_category):
-    print("Error! Only letters a-z allowed!")
-
+#User Interaction loop 
 while len(selected_books) == 0:
     browse_category = str(raw_input("Type the beginning of a category you would like to browse. ")).lower()
     
@@ -56,63 +58,46 @@ while len(selected_books) == 0:
     short_list = check(browse_category, check_categories)
     if len(short_list) == 0:
         print("We couldn't find a matching category.\n")
-        select_category = str(raw_input("Type the beginning of another category you would like to browse. ")).lower()
-        short_list = check(select_category, check_categories)
-        print(short_list)
 
     elif len(short_list) == 1:
-        print(short_list)
+        for category in short_list:
+            print(category.upper())
         select_category = str(raw_input("One category matches your search. Do you want to take a look at what's inside? (y or n): ")).lower()
         
         if select_category == 'y':
             #print books under that category
-            print("Okay, below are the books that match {0}: ".format(short_list[0]))
+            print("\nOkay, below are the books I have under {0}: \n".format(short_list[0].upper()))
             for book in pearls_bookshelf:
                 if book['category_lower'] == short_list[0]:
                     selected_books.append(book['title'] + " written by " + book['author'])
-            for i in selected_books:
+            print("Pearl's favorite book in {0} is: \n {1} \n".format(short_list[0].upper(),selected_books[0]))
+            print("Followed by: \n")
+            for i in selected_books[1:]:
                 print("{0}\n".format(i))
     
     elif len(short_list) > 1:
-        print(short_list)
+        for category in short_list:
+            print(category.upper())
         select_category = str(raw_input("We found a few categories that could fit your search. Give us a few more letters to narrow down what you're looking for: ")).lower()
         short_list = check(select_category, check_categories)
-        print(short_list)
+        for category in short_list:
+            print(category.upper())
         confirm_category = str(raw_input("Is this what you're looking for? (y or n): ")).lower()
 
         if confirm_category == 'y':
-            print("Okay, below are the books that match {0}: ".format(short_list[0]))
+            print("\nOkay, below are the books I have under {0}: \n".format(short_list[0].upper()))
             for book in pearls_bookshelf:
                 if book['category_lower'] == short_list[0]:
                     selected_books.append(book['title'] + " written by " + book['author'])
-            for i in selected_books:
+            print("Pearl's favorite book in {0} is: \n {1} \n".format(short_list[0].upper(),selected_books[0]))
+            print("Followed by: \n")
+            for i in selected_books[1:]:
                 print("{0}\n".format(i))
-
     
     repeat_loop = str(raw_input("Do you want to find other books? Enter y for yes and n for no. ")).lower()
     if repeat_loop == 'y':
         selected_books = []
+        
     else:
         print("Thanks for checking out Pearl's Bookshelf. We hope to see you again soon!")
         break
-        
-
-    
-   
-    
-
-#category_list = check(browse_category,check_categories)
-#category_str = " ".join([str(item) for item in category_list])
-
-
-#def binary_search(sorted_list, left_pointer, right_pointer, target):
-#    if left_pointer >= right_pointer:
-#       return 'Value not found'
-#    mid_idx = (left_pointer + right_pointer) // 2
-#    mid_val = sorted_list[mid_idx]
-#    if mid_val == target:
-#        return mid_idx
-#    if mid_val > target:
-#        return binary_search(sorted_list, left_pointer, mid_idx, target)
-#    if mid_val < target:
-#        return binary_search(sorted_list, mid_idx + 1, right_pointer, target)
